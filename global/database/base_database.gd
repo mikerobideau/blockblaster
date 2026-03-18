521bc926b790b4546e983f79d1f28c64b67b3f4e
Mike Robideau <kingrobideau@gmail.com>
2026-03-17 22:23:01 -0400

    query functions in shared BaseDatabase

--- global/database/base_database.gd
+++ global/database/base_database.gd
@@ -1,13 +1,15 @@
 extends RefCounted
 class_name BaseDatabase
 
+var data: Dictionary = {}
+
 static func load_directory(path: String) -> Dictionary:
-	var data: Dictionary = {}
+	var _data: Dictionary = {}
 	var dir = DirAccess.open(path)
 	
 	if not dir:
-		push_error("BaseDatabase: could not open path: " + path)
-		return data
+		push_error("Base_database: could not open path: " + path)
+		return _data
 	
 	dir.list_dir_begin()
 	var file = dir.get_next()
@@ -18,15 +20,31 @@ static func load_directory(path: String) -> Dictionary:
 			var res = load(full_path)
 			
 			if res == null:
-				push_warning("BaseDatabase: failed to load: " + full_path)
+				push_warning("Base_database: failed to load: " + full_path)
 			else:
 				var key = file.get_basename()
-				if data.has(key):
-					push_warning("BaseDatabase: duplicate key '" + key + "' skipping: " + full_path)
+				if _data.has(key):
+					push_warning("Base_database: duplicate key '" + key + "' skipping: " + full_path)
 				else:
-					data[key] = res
+					_data[key] = res
 		
 		file = dir.get_next()
 	
 	dir.list_dir_end()
-	return data
+	return _data
+
+func find_random() -> Resource:
+	var values = data.values()
+	return values[randi() % values.size()]
+	
+func find_by_type(type: int):
+	return find_by('type', type)
+
+func find_by(property: String, value: Variant) -> Resource:
+	var matches = data.values().filter(func(d): return d.get(property) == value)
+	if matches.is_empty():
+		push_warning("BaseDatabase: find_by found no matches for " + property + " = " + str(value))
+		return null
+	if matches.size() > 1:
+		push_warning("BaseDatabase: find_by found multiple matches for " + property + " = " + str(value))
+	return matches[0]
--- global/database/entity/formation_database.gd
+++ global/database/entity/formation_database.gd
@@ -1,11 +1,6 @@
-extends RefCounted
+extends BaseDatabase
 class_name FormationDatabase
 
-var data: Dictionary = {}
-
 func load_all():
-	data = BaseDatabase.load_directory("res://resource/formation/")
+	data = load_directory("res://resource/formation/")
 	
-func find_random():
-	var values = data.values()
-	return values[randi() % values.size()]
